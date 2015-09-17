3609bfdf4020f37506f1d73c8fb38e61a8baba90
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2015-09-17 18:04:28 -0500

    redes sociales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,6 @@ class ArticlesController < ApplicationController
     set_meta "title" => @article.title
     set_meta "viewport"   => "width=device-width, initial-scale=1"
     set_meta "og:title"   => @article.title
-    set_meta "og:description"   => Sanitize.fragment(@article.body.truncate(150), Sanitize::Config::RELAXED).html_safe
     set_meta "og:image"   => @article.image.url
     @article.update_visits_count
     if request.path != article_path(@article)
