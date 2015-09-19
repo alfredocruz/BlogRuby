a12724272c645b2198146546560d6481ab3dd248
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2015-09-18 19:32:42 -0500

    agregando base de datos

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,9 +46,11 @@ before_filter :configure_account_update_params, only: [:update]
    end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+  def configure_account_update_params
+     devise_parameter_sanitizer.for(:account_update) << :username
+     devise_parameter_sanitizer.for(:account_update) << :provider
+     devise_parameter_sanitizer.for(:account_update) << :uid
+   end
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
