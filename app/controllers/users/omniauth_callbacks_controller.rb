daa9e3e87ca948f97b82bd0976082382028947fc
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2015-09-18 20:14:08 -0500

    agregando base de datos

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,25 +25,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
-  include ApplicationHelper
+  
+  skip_before_filter :authenticate_user!
+
   def facebook
-    # Attempt to find the User
-    @user = User.find_for_facebook_oauth(
-      request.env["omniauth.auth"], 
-      current_user
-    )
-    
+    # You need to implement the method below in your model
+    @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
+
     if @user.persisted?
-      sign_in_and_redirect @user, 
-        :event => :authentication 
-      set_flash_message(:notice, 
-        :success, 
-        :kind => "Facebook") if is_navigational_format?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @user, :event => :authentication
     else
+      session["devise.facebook_data"] = env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
+  
   def passthru
+    logger.debug "Inside Passthrought"
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+    logger.debug "After render"
   end
 end
