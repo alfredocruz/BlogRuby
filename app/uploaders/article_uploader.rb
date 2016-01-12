5af1f21522f87110e6ea8f975445a1e95dd72944
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-12 15:21:14 -0600

    cambios hechos

--- app/uploaders/article_uploader.rb
+++ app/uploaders/article_uploader.rb
@@ -6,15 +6,15 @@ class ArticleUploader < CarrierWave::Uploader::Base
    include Cloudinary::CarrierWave
    #esta imagen es la del los recuadros del index
    version :index_cards do
-       process resize_to_fill: [80, 80,:north]
+       process resize_to_fill: [200, 100]
    end
    #primer dato anchura[anchura,altura]
    version :index_slider do
-       process resize_to_fill: [600, 400]
+       process resize_to_fill: [600, 300]
    end
    #imagen de articles show
    version :show do
-       process resize_to_fill: [200, 200]
+       process resize_to_fill: [300, 100]
    end
    # Crop to 275px, 206px
    version :thumbnail do
