8fa7b6cda573bcdd1bb28838efd49aa879bc7902
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-17 09:59:40 -0600

    cambios hechos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,14 +24,6 @@ $(document).on('ready page:load', function () {
       out_duration: 200, // Transition out duration
     }
     );
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5&appId=316086441848514";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 $(function() {
   prettyPrint();
   $('.lang-html').mouseover(function() {
