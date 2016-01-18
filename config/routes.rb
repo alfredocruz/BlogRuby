51cfd80a8016febaffb86a435cef12318675eea6
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-17 19:56:34 -0600

    cambios hechos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	get "sitemap" => "sitemaps#show"
   	resources :users, :path=>"usuarios_admin"
-  	get "(/:page)"=>"articles#index"
+  	get "/:page",to:"articles#index"
     resources :tags, :path => "category"
   	resources :articles, :path => "android"
   	get '/feed', to: 'articles#feed',:format => 'rss'
