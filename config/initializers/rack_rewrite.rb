dd18908ff9aae1735be87048c17f7542c077432e
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-19 12:50:53 -0600

    cambioando css

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 Apps::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-  r301 %r{.*}, 'http://www.yourdomainname.com$&', :if => Proc.new {|rack_env|
-    rack_env['SERVER_NAME'] != 'www.yourdomainname.com'
+  r301 %r{.*}, 'http://dowloadapps.herokuapp.com$&', :if => Proc.new {|rack_env|
+    rack_env['SERVER_NAME'] != 'www.dowloadapps.herokuapp.com/'
   }
 end if Rails.env == 'production'
