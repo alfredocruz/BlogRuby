fce7fd3a9cec3d9c217ccd20bf5e6a6f0dcc0718
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-21 22:02:43 -0600

    cambioando css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    
+    set_meta "robots" =>"all"
     set_meta "description"   => "Aplicaciones para Android, juegos , temas,herramientas, multimedia y mucho mejor que playstore"
     set_meta "title" => "dowloadapps-Aplicaciones para Android"
     set_meta "og:title"   => "dowloadapps"
@@ -26,10 +26,11 @@ class ArticlesController < ApplicationController
     set_meta "description"=> Sanitize.fragment(@article.markup_body.truncate(100), Sanitize::Config::RELAXED).html_safe
     set_meta "twitter:description"  => Sanitize.fragment(@article.markup_body.truncate(100), Sanitize::Config::RELAXED).html_safe
     set_meta "og:description"=> Sanitize.fragment(@article.markup_body.truncate(100), Sanitize::Config::RELAXED).html_safe
-    set_meta "title" => @article.title
+    set_meta "title" => @article.title+" Aplicaciones para Android"
+    set_meta "robots" =>"all"
     set_meta "og:title"   => @article.title
     set_meta "og:image"   => @article.image.url
-    set_meta "name" => @article.user.username
+    set_meta "author" => @article.user.username
     
     @article.update_visits_count
     if request.path != article_path(@article)
