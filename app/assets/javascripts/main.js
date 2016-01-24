572e8c2871ea141ecff4691bbd0f5bb18fccc155
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-23 19:28:44 -0600

    cambios en buscador

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,2 +1,34 @@
-function init(){$.stellar({horizontalScrolling:!1,hideDistantElements:!1})}$(document).on("ready page:load",function(){function e(e){var t=0;e.wheelDelta?t=e.wheelDelta/120:e.detail&&(t=-e.detail/3),l(t),e.preventDefault&&e.preventDefault(),e.returnValue=!1}function l(e){var l=1e3,t=400;$("html, body").stop().animate({scrollTop:$(window).scrollTop()-t*e},l)}$(".dropdown-button").dropdown(),$(".slider").slider({full_width:!0}),$(".button-collapse").sideNav(),init(),window.addEventListener&&window.addEventListener("DOMMouseScroll",e,!1),window.onmousewheel=document.onmousewheel=e,$("#flash-message").on("click",".close",function(){$(this).parent("#flash-message").fadeOut(250)}),$("select").material_select();var t=[{selector:".cuadro-1",offset:200,callback:"globalFunction()"},{selector:".other-class",offset:200,callback:"globalFunction()"}];Materialize.scrollFire(t)});
- 
+
+
+$(document).on("ready page:load",function(){
+	function e(e){
+		var t=0;
+		e.wheelDelta?t=e.wheelDelta/120:
+		e.detail&&(t=-e.detail/3),l(t),
+		e.preventDefault&&e.preventDefault(),
+		e.returnValue=!1}
+
+		function l(e){
+			var l=1e3,t=400;
+			$("html, body")
+			.stop().animate({scrollTop:
+				$(window)
+				.scrollTop()-t*e},l)
+		}
+		$(".dropdown-button").dropdown(),
+		$(".slider").slider({full_width:!0}),
+		// Initialize collapse button 
+		$(".button-collapse").sideNav(),
+
+		window.addEventListener&&window.addEventListener("DOMMouseScroll",e,!1),window.onmousewheel=document.onmousewheel=e,
+		$("#flash-message").on("click",".close",function(){
+			$(this).parent("#flash-message").fadeOut(250)}),
+		$("select").material_select();
+
+		var t=[
+		{selector:"#android",offset:20,callback:'Materialize.showStaggeredList("#android")'},
+		{selector:"#android",offset:20,callback:"globalFunction()"}];
+		Materialize.scrollFire(t)
+
+
+	});
