75a9ad495a557084a324e65b84f75bda1953c002
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-01-24 20:16:36 -0600

    cambioando css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	get "page(/:page)" => "articles#index"
 	get "sitemap" => "sitemaps#show"
   	resources :users, :path=>"usuarios_admin"
     resources :tags, :path => "category"
