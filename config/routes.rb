7f672ea589caedafe285504e9822d9fb05d02752
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-02-14 21:29:39 -0600

    cambioando css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get "sitemap" => "sitemaps#show"
   	resources :users, :path=>"usuarios_admin"
     resources :tags, :path => "category"
-  	resources :articles, :path => ""
+  	resources :articles, :path => "android"
   	get '/feed', to: 'articles#feed',:format => 'rss'
     devise_for :users
     root 'articles#index'
