a52bc783f9e2f92e91a2c69d3d6a4f90389a94e1
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-05-04 12:40:52 -0500

    cambioando css

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,2 +1,8 @@
-class SitemapsController < ApplicationController
+class SitemapsController 
+  def show
+    @articles = Article.where(:active => true) #we are generating url's for posts
+    respond_to do |format|
+     format.xml
+    end
+  end
 end
