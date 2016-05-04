c83ae0cb978be97ba951c33b714df7d987c0e359
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-05-04 13:06:23 -0500

    cambioando css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 	resources :sitemaps, :only => :show
 	get "sitemap" => "sitemaps#show"
-	
+
 	get "android_all"=>"articles"
 	get "page(/:page)" => "articles#index"
-	get "sitemap" => "sitemaps#show"
   	resources :users, :path=>"usuarios_admin"
     resources :tags, :path => "category"
   	resources :articles, :path => "android"
