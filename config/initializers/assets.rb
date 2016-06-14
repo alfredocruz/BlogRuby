329c9326d49a4302f528ed2dd98b69be08bb2d98
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-14 15:48:11 -0500

    cambios en index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.non_digested_assets ||= []  
-Rails.application.config.assets.non_digested_assets << 'materialize/font/**/*'
+Rails.application.config.assets.precompile << /materialize\/font\/.+\.(?:svg|eot|woff|woff2|ttf)\z/
