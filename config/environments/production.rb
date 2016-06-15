63282bb90eed378b29fa6f8669897c8b17f7e0c8
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-15 11:29:49 -0500

    cambios

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
   # Code is not reloaded between requests.
   config.cache_classes = true
   GA.tracker="UA-72483994-1"
-  GA.script_source = :doubleclick
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
