510cf824e4bb0549b2c12f4d725eb974c949fccf
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-15 13:35:29 -0500

    cambios en el servidor

--- config/initializers/compression.rb
+++ config/initializers/compression.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.debug = false
 
-  config.assets.js_compressor = uglifier
+  config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
   config.middleware.use Rack::Deflater
