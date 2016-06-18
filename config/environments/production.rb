23ed1b6c677d8abf9624209ff8a4b188b1c62a1e
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-18 16:32:26 -0500

    agregando css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
