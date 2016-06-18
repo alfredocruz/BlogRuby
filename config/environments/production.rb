02e9261bba71f295b4d503060f16c612bde2b173
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-18 16:49:35 -0500

    agregando css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,19 @@ config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'http://dowloadapps.herokuapp.com/'}
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
-  #    address: "smtp.gmail.com",
-  #    port: 587,
-  #    authentication: "plain",
-  #    enable_starttls_auto: true,
-  #    user_name: "alfredocruz9292@gmail.com",
-  #    password: "cruzhernandez" 
-  #}
+  config.action_mailer.default_url_options = { host: 'https://dowloadapps.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'alfredocruz9292@gmail.com',
+      password:             'cruzhernandez',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
