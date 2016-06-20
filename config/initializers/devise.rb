21b0e1ca60b031cd4ea46f6a1b199eab144e3571
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-20 15:26:24 -0500

    agregando auth facebook tweter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,8 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
-
+  config.omniauth :twitter, 'sAeOFWfXGrzOjoyI8u35yQ1qG','TXmHtN8pq1isNoIzznPiuEXK7GnVfQtAFm6mAdFd2DZRNbpJmA'  
+  config.omniauth :facebook, '316086441848514', '75505550d38278a172abc97938a20646', {:scope => 'email,user_birthday,read_stream'}  
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
@@ -259,6 +260,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :twitter, 'sAeOFWfXGrzOjoyI8u35yQ1qG','TXmHtN8pq1isNoIzznPiuEXK7GnVfQtAFm6mAdFd2DZRNbpJmA'  
-  config.omniauth :facebook, '316086441848514', '75505550d38278a172abc97938a20646', {:scope => 'email,read_stream'}  
+  
 end
