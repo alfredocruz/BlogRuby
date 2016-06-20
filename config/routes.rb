35736b2592686cf4b80309db5b17535aced00438
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-20 16:28:11 -0500

    agregando auth facebook tweter

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   	get '/feed', to: 'articles#feed',:format => 'rss'
 
     resources :authentications
-    devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout',sign_up: 'registration' },
+    devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout' },
     controllers:{omniauth_callbacks: "authentications",registrations: "registrations"}
     get '/entrar', to: 'authentications#index', as: 'entrar'
     root 'articles#index'
