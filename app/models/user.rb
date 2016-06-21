7621f178ef8f97fd3782fa08a0539a1477335fbc
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-21 11:14:37 -0500

    agregando css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-	TEMP_EMAIL_PREFIX = 'change@me'
-  	TEMP_EMAIL_REGEX = /\Achange@me/
+	
 	has_many :identities,  dependent: :destroy 
   	# Include default devise modules. Others available are:
   	# :confirmable, :lockable, :timeoutable and :omniauthable
@@ -8,38 +7,36 @@ class User < ActiveRecord::Base
 		  	:rememberable, :trackable, :validatable, :omniauthable
 
   	has_many :articles
-  	validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
+
 
   	def self.find_for_oauth(auth, signed_in_resource = nil)
     identity = Identity.find_for_oauth(auth)
     user = signed_in_resource ? signed_in_resource : identity.user
 
     if user.nil?
-      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      email = auth.info.email if email_is_verified
+      email = auth.info.email
       username = auth.extra.raw_info.name
-      user = User.where(email: email).first if email
+      user = User.find_by(email: email) if email
 
       # Create the user if it's a new registration
       if user.nil?
         password = Devise.friendly_token[0,20]
         if auth.provider == 'facebook'
           user = User.new(
-            email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+            email: email ? email : "#{auth.uid}@change-me.com",
             password: password,
             username: username,
             password_confirmation: password
           )
         elsif auth.provider == 'twitter'
           user = User.new(
-           	email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+            email: "#{auth.uid}@change-me.com",
             password: password,
             password_confirmation: password
           )
         end
-        user.skip_confirmation!
-        user.save!
       end
+      user.save!
     end
 
     if identity.user != user
@@ -51,6 +48,14 @@ class User < ActiveRecord::Base
   end
 
   def email_verified?
-    self.email && self.email !~ TEMP_EMAIL_REGEX
+    if self.email
+      if self.email.split('@')[1] == 'change-me.com'
+        return false
+      else
+        return true
+      end
+    else
+      return false
+    end
   end
 end
