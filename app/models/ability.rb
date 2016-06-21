59e2db3759f7fa794f723e40c9f8734c77bebe4f
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-21 12:17:50 -0500

    agregando css

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
     def initialize(user)
     # Define abilities for the passed in user here. For example:
         user ||= User.new # guest user (not logged in)
-        if user.email == "alfredo_92-ca@hotmail.com"
+        if user.role == "admin"
             can :manage, :all
         elsif user.role == ("autor")
             can :read, Tag
