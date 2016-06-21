2a3eaeddfbea6ff234e24855b7e54ac14230c54e
Alfredo Cruz <alfredo_92-ca@hotmail.com>
2016-06-20 22:39:20 -0500

    agregando mas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
 
   	def finish_signup
 	    if request.patch? && params[:user] # Revisa si el request es de tipo patch, es decir, llenaron el formulario y lo ingresaron
-	      @user = User.find(params[:id])
 
 	      if @user.update(user_params)
 	        sign_in(@user, :bypass => true)
